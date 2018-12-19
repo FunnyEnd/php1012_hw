714aee6a3a69feadb282938d9a03bba3b19cc3c9
FunnyEnd <kornienko_aa@yahoo.com>
2018-12-19 15:22:53 +0200

    edit

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
 spl_autoload_register(function ($class) {
-    $includeClassName = str_replace('\\', '/', $class . ".php");
-    if (file_exists($includeClassName))
-        require $includeClassName;
+  $includeClassName = str_replace('\\', '/', $class . ".php");
+  if (file_exists($includeClassName))
+    require $includeClassName;
 
 });
 
 set_error_handler(
-    function ($errorType, $errorText, $errfile, $errline){
-        $errorTime = date("H:i:m");
-        $backtrace = debug_backtrace();
-        $backtraceStr = "";
-        foreach ($backtrace as $b){
-            if($b['function'] == '{closure}')
-                continue;
+    function ($errorType, $errorText, $errfile, $errline) {
+      $errorTime = date("H:i:m");
+      $backtrace = debug_backtrace();
+      $backtraceStr = "";
+      foreach ($backtrace as $b) {
+        if ($b['function'] == '{closure}')
+          continue;
 
-            $backtraceStr .= $b['file'] . " at line " . $b['line'] . ". Call function " . $b['function'] . "(); \n";
-        }
-        $res = "[$errorTime] {$errorType}. $errorText $errfile at line $errline \n$backtraceStr \n";
-        file_put_contents('error-log.txt', $res, FILE_APPEND);
-});
+        $backtraceStr .= $b['file'] . " at line " . $b['line'] . ". Call function " . $b['function'] . "(); \n";
+      }
+      $res = "[$errorTime] {$errorType}. $errorText $errfile at line $errline \n$backtraceStr \n";
+      file_put_contents('error-log.txt', $res, FILE_APPEND);
+    });
 
-set_exception_handler(function($e) {
-    $errorTime = date("H:i:m");
-    $backtrace = $e->getTraceAsString();
-    $res = "[$errorTime] Uncaught exception. " . $e->getMessage() . $backtrace . "\n";
-    file_put_contents('error-log.txt', $res, FILE_APPEND);
+set_exception_handler(function (Throwable $e) {
+  $errorTime = date("H:i:m");
+  $backtrace = $e->getTraceAsString();
+  $res = "[$errorTime] Uncaught exception. " . $e->getMessage() . $backtrace . "\n";
+  file_put_contents('error-log.txt', $res, FILE_APPEND);
 });
 
 $application = new \Framework\ApplicationKernel\Application();
