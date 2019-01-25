8ef4bf455b6a2fb1a9de0f06adec5c8e2a72dccc
FunnyEnd <kornienko_aa@yahoo.com>
2019-01-25 17:14:08 +0200

    refactor constant class.

--- framework/Constants.php
+++ framework/Constants.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phpstudent
- * Date: 09.01.19
- * Time: 16:39
- */
 
 namespace Framework;
-
 
 class Constants
 {
     public const DATETIME_FORMAT = "Y-m-d H:i:s";
-
 }
